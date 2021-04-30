d13f9585ae08bb888c522b60d7d03d1b86ba4a26
mc17336 <corriematt@ymail.com>
2021-04-30 13:03:05 +0100

    Update NCSC-Report-Parallelisation.py

--- Matt Corrie/NCSC-Report-Parallelisation.py
+++ Matt Corrie/NCSC-Report-Parallelisation.py
@@ -16,10 +16,10 @@ from functools import reduce
 from multiprocessing import Pool, cpu_count, current_process
 import datetime
 
-# creating the function to get title, articles and tags        
+# creating the function to get title, articles and tags
 
 def get_articles(link):
-    
+
     chrome_options = Options()
     chrome_options.add_argument("headless")
     chrome_options.add_argument("no-sandbox")
@@ -27,10 +27,10 @@ def get_articles(link):
     gbrowser = webdriver.Chrome(executable_path='chromedriver.exe',chrome_options=chrome_options)
     gbrowser.get(link)
     WebDriverWait(gbrowser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "pcf-title")))
-    
+
     articles = []
     t = []
-    
+
     try:
 
         title = gbrowser.find_elements_by_class_name("pcf-title")
@@ -58,57 +58,63 @@ def get_articles(link):
 
     except:
         pass
-    
+
     data = {'Title': title,'Article': articles,'topics': t}
     gbrowser.close()
-    
+
     return data
+
+def get_links(main_link):
+
+        # setting options
+        chrome_options = Options()
+        chrome_options.add_argument("headless")
+        chrome_options.add_argument("no-sandbox")
+        chrome_options.add_argument("disable-gpu")
+
+        # getting links to be looped over
+        linkbrowser = webdriver.Chrome(executable_path='chromedriver.exe',chrome_options = chrome_options)
+        linkbrowser.get(main_link)
+
+        links=[]
+
+        if links == []:
+            WebDriverWait(linkbrowser, 10).until(EC.presence_of_element_located((By.XPATH, ".//a")))
+
+            for a in linkbrowser.find_elements_by_xpath('.//a'):
+                links.append(a.get_attribute('href'))
+        else:
+            linkbrowser.close()
+
+        report_links = []
+        for ind, link in enumerate(links):
+            if 'report/weekly' in link:
+                report_links.append(link)
+
+        return report_links
 
 # creating our multi-processing
 
 if __name__ == "__main__":
-    
+
     begin_time = datetime.datetime.now()
     # print the number of cores
     print("Number of cores available equals %s" % cpu_count())
     print("Using %s cores" % 16)
-    
-    # setting options
-    chrome_options = Options()
-    chrome_options.add_argument("headless")
-    chrome_options.add_argument("no-sandbox")
-    chrome_options.add_argument("disable-gpu")
 
-    # getting links to be looped over
-    
-    linkbrowser = webdriver.Chrome(executable_path='chromedriver.exe',chrome_options = chrome_options)
-    linkbrowser.get('https://www.ncsc.gov.uk/section/keep-up-to-date/threat-reports?q=&defaultTypes=report&sort=date%2Bdesc&writtenFor=Large+organisations&rows=232')
+    main_link = 'https://www.ncsc.gov.uk/section/keep-up-to-date/threat-reports?q=&defaultTypes=report&sort=date%2Bdesc&writtenFor=Large+organisations&rows=232'
 
-    links=[]
-
-    if links == []:
-        WebDriverWait(linkbrowser, 10).until(EC.presence_of_element_located((By.XPATH, ".//a")))
-
-        for a in linkbrowser.find_elements_by_xpath('.//a'):
-            links.append(a.get_attribute('href'))
-    else:
-        linkbrowser.close()
-
-    # isolating links that are weekly reports
-
-    report_links = []
-    for ind, link in enumerate(links):
-        if 'report/weekly' in link:
-            report_links.append(link)
-    
     # create a pool of workers
     # start all worker processes
-    pool = Pool(processes= cpu_count()) 
-    
+    pool = Pool(processes= cpu_count())
+
+    with pool as p:
+        report_links = get_links(main_link)
+        
     # context manager so no need to close
     with pool as p:
         results = p.map(get_articles, [link for link in report_links])
-    
+
     print("Finished getting articles!")
     end_time = datetime.datetime.now()
     print(end_time - begin_time)
